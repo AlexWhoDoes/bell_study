d35ac6d7726cba16c51d98dcab26ff0c2aaa59e4
alex <mr.shevchenkoav@mail.ru>
2021-03-24 21:06:00 +0300

    Script for initializing the db's schema modified Data added into DB Application.properties file added hibernate.properties file deleted

--- src/main/java/resources/data.sql
+++ src/main/java/resources/data.sql
@@ -1,11 +1,37 @@
-INSERT INTO House (id, version, address) VALUES (1, 0, 'ул.Цюрупы, 16');
+INSERT INTO Citizenship (code, citizenship_name)
+VALUES ('001', 'Russian federation'),
+       ('002', 'Ukraine'),
+       ('003', 'USA'),
+       ('004', 'UK');
 
-INSERT INTO House (id, version, address) VALUES (2, 0, 'ул.Лунина, 7');
+INSERT INTO Document_type (code, document_name)
+VALUES ('001', 'Passport'),
+       ('002', 'Driver license');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (1, 0, 'Пётр', 20);
+INSERT INTO Document (document, citizenship, document_date)
+VALUES (1, 1, '2007-01-01'),
+       (1, 2, '2009-01-02'),
+       (2, 3, '2014-02-03'),
+       (1, 4, '2013-04-03'),
+       (2, 1, '2020-01-05');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (2, 0, 'John', 25);
+INSERT INTO Organization (short_name, full_name, inn, kpp, address, phone, is_active)
+VALUES ('Firstbank', 'LLC_First_bank ', '1234567890', '987654321', 'S.Petersburg, Green street, 54', '9897785432', true),
+       ('Secondbank', 'LLC_Second_bank ', '0987654321', '123456789', 'Moscow, Black street, 1', '9897785432', true);
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 1);
+INSERT INTO Office (office_name, address, phone, isActive, id_organisation)
+VALUES ('Head_office', 'Moscow, Proletarsky district, 2-1-8', '4959999958', true, 1),
+       ('Just_office', 'Moscow, Kievskiy district, 7', '4959999957', true, 1),
+       ('One_more_office', 'S.Petersburg, River district, 24', '369988957', true, 1),
+       ('Head_office', 'S.Petersburg, Field district, 44-55', '369879954', true, 2),
+       ('Just_office', 'London, Biker street, 44-55', '44488996', true, 2);
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 2);
+INSERT INTO User (first_name, second_name, middle_name, position, phone, document, is_identified, id_office)
+VALUES ('Alexander', 'Vinogradov', 'Dmitrievich', 'CIO', '9999453378', 1, true, 1),
+       ('Vladimir', 'Vinogradov', 'Dmitrievich', 'CTO', '9864563221', 1, false, 2),
+       ('Alexey', 'Lobanov', 'Ivanovich', 'COO', '9997876532', 2, true, 3),
+       ('Max', 'Smith', 'Black', 'accountant', '88898965', 1, false, 4),
+       ('John', 'Doe', 'J', 'unknown', '00000000', 1, false, 1);
+
+
+
--- src/main/java/resources/schema.sql
+++ src/main/java/resources/schema.sql
@@ -1,6 +1,32 @@
+CREATE TABLE IF NOT EXISTS Citizenship (
+    id             INTEGER                            COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    code           VARCHAR(3)    NOT NULL             COMMENT 'Citizenship code',
+    citizenship_name     VARCHAR(50)   NOT NULL       COMMENT 'Citizenship name'
+);
+COMMENT ON TABLE Citizenship IS 'directory of citizenship';
+
+CREATE TABLE IF NOT EXISTS Document_type (
+    id             INTEGER                      COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    code           VARCHAR(3)    NOT NULL       COMMENT 'Document code',
+    document_name  VARCHAR(50)   NOT NULL       COMMENT 'Document name'
+);
+COMMENT ON TABLE Document_type IS 'directory of document types';
+
+CREATE TABLE IF NOT EXISTS Document (
+     id              INTEGER                     COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+     document        INTEGER     NOT NULL        COMMENT 'Real document',
+     citizenship     INTEGER     NOT NULL        COMMENT 'Real citizenship',
+     document_date   DATE        NOT NULL        COMMENT 'Real document date'
+);
+COMMENT ON TABLE Document IS 'real documents table';
+ALTER TABLE Document ADD FOREIGN KEY (document) REFERENCES Document_type(id);
+ALTER TABLE Document ADD FOREIGN KEY (citizenship) REFERENCES Citizenship(id);
+CREATE INDEX IX_User_citizenship ON Document (citizenship);
+CREATE INDEX IX_User_document ON Document (document);
+
 CREATE TABLE IF NOT EXISTS Organization (
     id         INTEGER                         COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(50)   NOT NULL          COMMENT 'Short name of an organization',
+    short_name VARCHAR(50)   NOT NULL          COMMENT 'Short name of an organization',
     full_name  VARCHAR(150)  NOT NULL          COMMENT 'Full name of an organization',
     inn        VARCHAR(10)   NOT NULL          COMMENT 'Organization TIN',
     kpp        VARCHAR(9)    NOT NULL          COMMENT 'Organization RRC',
@@ -9,11 +35,11 @@ CREATE TABLE IF NOT EXISTS Organization (
     is_active  BOOLEAN       DEFAULT TRUE      COMMENT 'Organization status'
 );
 CREATE INDEX IX_Organization_id ON Organization (id);
-CREATE INDEX IX_Organization_name ON Organization (name);
+CREATE INDEX IX_Organization_name ON Organization (short_name);
 
 CREATE TABLE IF NOT EXISTS Office (
     id              INTEGER                    COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-    name            VARCHAR(50)   NOT NULL     COMMENT 'Short name of an organization office',
+    office_name     VARCHAR(50)   NOT NULL     COMMENT 'Short name of an organization office',
     address         TEXT          NOT NULL     COMMENT 'Address of an organization office',
     phone           VARCHAR(10)                COMMENT 'Phone number of an organization office',
     isActive        BOOLEAN       DEFAULT TRUE COMMENT 'Status of an organization office ',
@@ -37,32 +63,6 @@ CREATE TABLE IF NOT EXISTS User (
 ALTER TABLE User ADD FOREIGN KEY (id_office) REFERENCES Office(id);
 ALTER TABLE User ADD FOREIGN KEY (document)  REFERENCES Document(id);
 CREATE INDEX IX_User_id ON User (id);
-CREATE INDEX IX_User_id ON User (first_name);
+CREATE INDEX IX_User_first_name ON User (first_name);
 CREATE INDEX IX_User_id_office ON User (id_office);
 CREATE INDEX IX_User_id_document ON User (document);
-
-CREATE TABLE IF NOT EXISTS Citizenship (
-    id             INTEGER                      COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-    code           VARCHAR(3)    NOT NULL       COMMENT 'Citizenship code',
-    name           VARCHAR(50)   NOT NULL       COMMENT 'Citizenship name'
-);
-COMMENT ON TABLE Citizenship IS 'directory of citizenship';
-
-CREATE TABLE IF NOT EXISTS Document_type (
-    id             INTEGER                      COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-    code           VARCHAR(3)    NOT NULL       COMMENT 'Document code',
-    document_name  VARCHAR(50)   NOT NULL       COMMENT 'Document name'
-);
-COMMENT ON TABLE Document_type IS 'directory of document types';
-
-CREATE TABLE IF NOT EXISTS Document (
-     id              INTEGER                     COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-     document        INTEGER     NOT NULL        COMMENT 'Real document',
-     citizenship     INTEGER     NOT NULL        COMMENT 'Real citizenship',
-     date            DATE        NOT NULL        COMMENT 'Real document date'
-);
-COMMENT ON TABLE Document IS 'real documents table';
-ALTER TABLE Document ADD FOREIGN KEY (document) REFERENCES Document_type(id);
-ALTER TABLE Document ADD FOREIGN KEY (citizenship) REFERENCES Citizenship(id);
-CREATE INDEX IX_User_citizenship ON Document (citizenship);
-CREATE INDEX IX_User_document ON Document (document);
