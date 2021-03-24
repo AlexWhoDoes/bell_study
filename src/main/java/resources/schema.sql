bcd6c6fd8638951ac2456794778d1fb10f130567
alex <mr.shevchenkoav@mail.ru>
2021-03-24 10:31:35 +0300

    DB's schema added

--- src/main/java/resources/schema.sql
+++ src/main/java/resources/schema.sql
@@ -1,28 +1,68 @@
-CREATE TABLE IF NOT EXISTS Person (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    first_name VARCHAR(50) NOT NULL COMMENT 'Имя',
-    age        INTEGER  NOT NULL    COMMENT 'Возраст'
+CREATE TABLE IF NOT EXISTS Organization (
+    id         INTEGER                         COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50)   NOT NULL          COMMENT 'Short name of an organization',
+    full_name  VARCHAR(150)  NOT NULL          COMMENT 'Full name of an organization',
+    inn        VARCHAR(10)   NOT NULL          COMMENT 'Organization TIN',
+    kpp        VARCHAR(9)    NOT NULL          COMMENT 'Organization RRC',
+    address    TEXT          NOT NULL          COMMENT 'Head office organization address',
+    phone      VARCHAR(10)                     COMMENT 'Head office organization phone',
+    is_active  BOOLEAN       DEFAULT TRUE      COMMENT 'Organization status'
 );
-COMMENT ON TABLE Person IS 'Человек';
+CREATE INDEX IX_Organization_id ON Organization (id);
+CREATE INDEX IX_Organization_name ON Organization (name);
 
-CREATE TABLE IF NOT EXISTS House (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
+CREATE TABLE IF NOT EXISTS Office (
+    id              INTEGER                    COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(50)   NOT NULL     COMMENT 'Short name of an organization office',
+    address         TEXT          NOT NULL     COMMENT 'Address of an organization office',
+    phone           VARCHAR(10)                COMMENT 'Phone number of an organization office',
+    isActive        BOOLEAN       DEFAULT TRUE COMMENT 'Status of an organization office ',
+    id_organisation INTEGER       NOT NULL     COMMENT 'Unique identifier of an organization to which an office belongs'
 );
-COMMENT ON TABLE House IS 'Дом';
+ALTER TABLE Office ADD FOREIGN KEY (id_organisation) REFERENCES Organization(id);
+CREATE INDEX IX_Office_id ON Office (id);
+CREATE INDEX IX_Office_id_organisation ON Office (id_organisation);
 
-CREATE TABLE IF NOT EXISTS Person_House (
-    person_id   INTEGER  NOT NULL COMMENT 'Уникальный идентификатор человека',
-    house_id    INTEGER  NOT NULL COMMENT 'Уникальный идентификатор дома',
-
-    PRIMARY KEY (person_id, house_id)
+CREATE TABLE IF NOT EXISTS User (
+    id               INTEGER                    COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    first_name       VARCHAR(50)   NOT NULL     COMMENT 'User name',
+    second_name      VARCHAR(50)                COMMENT 'User surname',
+    middle_name      VARCHAR(50)                COMMENT 'User middle name',
+    position         VARCHAR(50)   NOT NULL     COMMENT 'User position',
+    phone            VARCHAR(10)                COMMENT 'User phone',
+    document         INTEGER                    COMMENT 'User document',
+    is_identified    BOOLEAN       DEFAULT TRUE COMMENT 'Is user identified?',
+    id_office        INTEGER       NOT NULL     COMMENT 'Unique identifier of an organization to which a user belongs'
 );
-COMMENT ON TABLE Person_House IS 'join-таблица для связи человека и дома';
+ALTER TABLE User ADD FOREIGN KEY (id_office) REFERENCES Office(id);
+ALTER TABLE User ADD FOREIGN KEY (document)  REFERENCES Document(id);
+CREATE INDEX IX_User_id ON User (id);
+CREATE INDEX IX_User_id ON User (first_name);
+CREATE INDEX IX_User_id_office ON User (id_office);
+CREATE INDEX IX_User_id_document ON User (document);
 
-CREATE INDEX IX_Person_House_Id ON Person_House (house_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (house_id) REFERENCES House(id);
+CREATE TABLE IF NOT EXISTS Citizenship (
+    id             INTEGER                      COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    code           VARCHAR(3)    NOT NULL       COMMENT 'Citizenship code',
+    name           VARCHAR(50)   NOT NULL       COMMENT 'Citizenship name'
+);
+COMMENT ON TABLE Citizenship IS 'directory of citizenship';
 
-CREATE INDEX IX_House_Person_Id ON Person_House (person_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (person_id) REFERENCES Person(id);
+CREATE TABLE IF NOT EXISTS Document_type (
+    id             INTEGER                      COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    code           VARCHAR(3)    NOT NULL       COMMENT 'Document code',
+    document_name  VARCHAR(50)   NOT NULL       COMMENT 'Document name'
+);
+COMMENT ON TABLE Document_type IS 'directory of document types';
+
+CREATE TABLE IF NOT EXISTS Document (
+     id              INTEGER                     COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+     document        INTEGER     NOT NULL        COMMENT 'Real document',
+     citizenship     INTEGER     NOT NULL        COMMENT 'Real citizenship',
+     date            DATE        NOT NULL        COMMENT 'Real document date'
+);
+COMMENT ON TABLE Document IS 'real documents table';
+ALTER TABLE Document ADD FOREIGN KEY (document) REFERENCES Document_type(id);
+ALTER TABLE Document ADD FOREIGN KEY (citizenship) REFERENCES Citizenship(id);
+CREATE INDEX IX_User_citizenship ON Document (citizenship);
+CREATE INDEX IX_User_document ON Document (document);
