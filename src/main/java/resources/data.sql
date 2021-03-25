f4d9849f516cc3655ac5b5426a663ea4028b356d
alex <mr.shevchenkoav@mail.ru>
2021-03-25 12:48:15 +0300

    Foreign name changed. Document number column added into Document table.

--- src/main/java/resources/data.sql
+++ src/main/java/resources/data.sql
@@ -8,12 +8,12 @@ INSERT INTO Document_type (code, document_name)
 VALUES ('001', 'Passport'),
        ('002', 'Driver license');
 
-INSERT INTO Document (document, citizenship, document_date)
-VALUES (1, 1, '2007-01-01'),
-       (1, 2, '2009-01-02'),
-       (2, 3, '2014-02-03'),
-       (1, 4, '2013-04-03'),
-       (2, 1, '2020-01-05');
+INSERT INTO Document (doc_type_id, citizenship_id, document_date, document_number)
+VALUES (1, 1, '2007-01-01', '778-887'),
+       (1, 2, '2009-01-02', '748-447'),
+       (2, 3, '2014-02-03', '798-887'),
+       (1, 4, '2013-04-03', '987-876'),
+       (2, 1, '2020-01-05', '984-998');
 
 INSERT INTO Address (city, street, house)
 VALUES ('Moscow', 'Proletarsky district', '20'),
@@ -24,18 +24,18 @@ VALUES ('Moscow', 'Proletarsky district', '20'),
        ('London', 'Biker street', '24'),
        ('Boston', 'Black street', '65');
 
-INSERT INTO Organization (short_name, full_name, inn, kpp, address, phone, is_active)
+INSERT INTO Organization (short_name, full_name, inn, kpp, address_id, phone, is_active)
 VALUES ('Firstbank', 'LLC_First_bank ', '1234567890', '987654321', 1, '9897785432', true),
        ('Secondbank', 'LLC_Second_bank ', '0987654321', '123456789', 2, '9897785432', true);
 
-INSERT INTO Office (office_name, address, phone, isActive, id_organisation)
+INSERT INTO Office (office_name, address_id, phone, isActive, id_organisation)
 VALUES ('Head_office', 3, '4959999958', true, 1),
        ('Just_office', 4, '4959999957', true, 1),
        ('One_more_office', 5, '369988957', true, 1),
        ('Head_office', 6, '369879954', true, 2),
        ('Just_office', 7, '44488996', true, 2);
 
-INSERT INTO User (first_name, second_name, middle_name, position, phone, document, is_identified, id_office)
+INSERT INTO User (first_name, second_name, middle_name, position, phone, document_id, is_identified, id_office)
 VALUES ('Alexander', 'Vinogradov', 'Dmitrievich', 'CIO', '9999453378', 1, true, 1),
        ('Vladimir', 'Vinogradov', 'Dmitrievich', 'CTO', '9864563221', 1, false, 2),
        ('Alexey', 'Lobanov', 'Ivanovich', 'COO', '9997876532', 2, true, 3),
--- src/main/java/resources/schema.sql
+++ src/main/java/resources/schema.sql
@@ -14,15 +14,16 @@ COMMENT ON TABLE Document_type IS 'directory of document types';
 
 CREATE TABLE IF NOT EXISTS Document (
      id              INTEGER                     COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-     document        INTEGER     NOT NULL        COMMENT 'Real document',
-     citizenship     INTEGER     NOT NULL        COMMENT 'Real citizenship',
-     document_date   DATE        NOT NULL        COMMENT 'Real document date'
+     doc_type_id     INTEGER     NOT NULL        COMMENT 'Real document',
+     citizenship_id  INTEGER     NOT NULL        COMMENT 'Real citizenship',
+     document_date   DATE        NOT NULL        COMMENT 'Real document date',
+     document_number VARCHAR(10) NOT NULL        COMMENT 'Real document number'
 );
 COMMENT ON TABLE Document IS 'real documents table';
-ALTER TABLE Document ADD FOREIGN KEY (document) REFERENCES Document_type(id);
-ALTER TABLE Document ADD FOREIGN KEY (citizenship) REFERENCES Citizenship(id);
-CREATE INDEX IX_User_citizenship ON Document (citizenship);
-CREATE INDEX IX_User_document ON Document (document);
+ALTER TABLE Document ADD FOREIGN KEY (doc_type_id) REFERENCES Document_type(id);
+ALTER TABLE Document ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
+CREATE INDEX IX_User_citizenship ON Document (citizenship_id);
+CREATE INDEX IX_User_document ON Document (doc_type_id);
 
 CREATE TABLE IF NOT EXISTS Address (
     id         INTEGER                         COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
@@ -37,24 +38,24 @@ CREATE TABLE IF NOT EXISTS Organization (
     full_name  VARCHAR(150)  NOT NULL          COMMENT 'Full name of an organization',
     inn        VARCHAR(10)   NOT NULL          COMMENT 'Organization TIN',
     kpp        VARCHAR(9)    NOT NULL          COMMENT 'Organization RRC',
-    address    INTEGER       NOT NULL          COMMENT 'Head office organization address',
+    address_id INTEGER       NOT NULL          COMMENT 'Head office organization address',
     phone      VARCHAR(10)                     COMMENT 'Head office organization phone',
     is_active  BOOLEAN       DEFAULT TRUE      COMMENT 'Organization status'
 );
-ALTER TABLE Organization ADD FOREIGN KEY (address) REFERENCES Address(id);
+ALTER TABLE Organization ADD FOREIGN KEY (address_id) REFERENCES Address(id);
 CREATE INDEX IX_Organization_address ON Address (id);
 CREATE INDEX IX_Organization_name ON Organization (short_name);
 
 CREATE TABLE IF NOT EXISTS Office (
     id              INTEGER                    COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
     office_name     VARCHAR(50)   NOT NULL     COMMENT 'Short name of an organization office',
-    address         INTEGER       NOT NULL     COMMENT 'Address of an organization office',
+    address_id      INTEGER       NOT NULL     COMMENT 'Address of an organization office',
     phone           VARCHAR(10)                COMMENT 'Phone number of an organization office',
     isActive        BOOLEAN       DEFAULT TRUE COMMENT 'Status of an organization office ',
     id_organisation INTEGER       NOT NULL     COMMENT 'Unique identifier of an organization to which an office belongs'
 );
 ALTER TABLE Office ADD FOREIGN KEY (id_organisation) REFERENCES Organization(id);
-ALTER TABLE Office ADD FOREIGN KEY (address) REFERENCES Address(id);
+ALTER TABLE Office ADD FOREIGN KEY (address_id) REFERENCES Address(id);
 CREATE INDEX IX_Office_address ON Address (id);
 CREATE INDEX IX_Office_id_organisation ON Office (id_organisation);
 
@@ -65,12 +66,12 @@ CREATE TABLE IF NOT EXISTS User (
     middle_name      VARCHAR(50)                COMMENT 'User middle name',
     position         VARCHAR(50)   NOT NULL     COMMENT 'User position',
     phone            VARCHAR(10)                COMMENT 'User phone',
-    document         INTEGER                    COMMENT 'User document',
+    document_id      INTEGER                    COMMENT 'User document',
     is_identified    BOOLEAN       DEFAULT TRUE COMMENT 'Is user identified?',
     id_office        INTEGER       NOT NULL     COMMENT 'Unique identifier of an organization to which a user belongs'
 );
 ALTER TABLE User ADD FOREIGN KEY (id_office) REFERENCES Office(id);
-ALTER TABLE User ADD FOREIGN KEY (document)  REFERENCES Document(id);
+ALTER TABLE User ADD FOREIGN KEY (document_id)  REFERENCES Document(id);
 CREATE INDEX IX_User_first_name ON User (first_name);
 CREATE INDEX IX_User_id_office ON User (id_office);
-CREATE INDEX IX_User_id_document ON User (document);
+CREATE INDEX IX_User_id_document ON User (document_id);
